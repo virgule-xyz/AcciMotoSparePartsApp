3c657672ba8808ca09699292d634dbce3db6c228
Pierre Canthelou <pierre@onzedix.fr>
2018-09-30 15:16:52 +0200

    recomposition avec HOC

--- App.js
+++ App.js
@@ -1,31 +1,34 @@
-import {
-    createSwitchNavigator,
-    createStackNavigator
-} from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import Screen1 from '@screens/Screen1';
 import Screen2 from '@screens/Screen2';
 import Screen3 from '@screens/Screen3';
 import Screen4 from '@screens/Screen4';
 import Screen5 from '@screens/Screen5';
 
-const ScreenStacks = createStackNavigator({
+const ScreenStacks = createStackNavigator(
+  {
     Screen2,
     Screen3,
     Screen4,
-    Screen5
-}, {
+    Screen5,
+  },
+  {
     initialRouteName: 'Screen2',
     navigationOptions: {
-        header: null,
+      header: null,
     },
-});
+  },
+);
 
-export default createSwitchNavigator({
+export default createSwitchNavigator(
+  {
     Screen1,
     ScreenStacks,
-}, {
+  },
+  {
     initialRouteName: 'Screen1',
     navigationOptions: {
-        header: null,
+      header: null,
     },
-});
+  },
+);
